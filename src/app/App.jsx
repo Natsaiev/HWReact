dceef0c60d13c2e35df238d7939035891a49723f
Natsaiev <natsaievv@gmail.com>
2024-09-10 22:06:17 +0200

    commit Main

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
-import { Rating } from "./Components";
-import { List } from "./Components";
+
 
 
 
 function App(){
 return(
     <div className="App">
-        <Rating/>
-        <List />
+
     </div>
 )
 
