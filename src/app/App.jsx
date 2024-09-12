16fca90b4fe6dca93b46aad252d915b10413f0dd
Natsaiev <natsaievv@gmail.com>
2024-09-12 23:21:25 +0200

    commit HW7

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,18 @@
-import "./App.css";
+import ButtonComponent from "./Components/ButtonComponent";
+import LangComponent from "./Components/LangComponent";
+import TextComponent from "./Components/TextComponent";
 
-
-
-
-function App(){
-return(
-    <div className="App">
-
+function App() {
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <LangComponent>
+        <TextComponent />
+        <ButtonComponent />
+      </LangComponent>
     </div>
-)
-
-
+  );
 }
 
 export default App;
