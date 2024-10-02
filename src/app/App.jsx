244698f0e31787c2dbbd3e6f15c5c804318dfc0b
Natsaiev <natsaievv@gmail.com>
2024-10-02 21:09:04 +0200

    first commit

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,53 @@
-import "./App.css";
+import { useCallback, useMemo, useState } from "react";
+import { UserList } from "./Components/UserList";
 
-
-
-
-function App(){
-return(
-    <div className="App">
-
+const users = [
+  {
+    id: 1,
+    name: "Ralph",
+  },
+  {
+    id: 2,
+    name: "Holly",
+  },
+  {
+    id: 3,
+    name: "Rose",
+  },
+  {
+    id: 4,
+    name: "George",
+  },
+  {
+    id: 5,
+    name: "Neva",
+  },
+];
+function App() {
+  const [filter, setFilter] = useState("");
+  const filterUsers = useCallback((filter) => {
+    if (filter.length !== 0) {
+      const filteredArray = users.filter((user) =>
+        user.name.toLowerCase().includes(filter.toLowerCase())
+      );
+      return filteredArray;
+    }
+    return users;
+  }, []);
+  const filteredArray = useMemo(
+    () => filterUsers(filter),
+    [filter, filterUsers]
+  );
+  return (
+    <div>
+      <input
+        type="text"
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <UserList userArray={filteredArray} />
     </div>
-)
-
-
+  );
 }
 
 export default App;
