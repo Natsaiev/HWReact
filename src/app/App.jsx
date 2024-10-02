5431d9b838aef8d2e5885bbbefd6d4bce0edf689
Natsaiev <natsaievv@gmail.com>
2024-10-02 21:28:35 +0200

    first commit

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,9 @@
-import "./App.css";
+import { MyForm } from "./Components/MyForm";
 
-
-
-
-function App(){
-return(
-    <div className="App">
-
-    </div>
-)
-
-
+function App() {
+  return <>
+    <MyForm />
+  </>;
 }
 
 export default App;
